2ca2160ee5f2cb5d4769415225e0bf60e6f910e5
Dennis Svatunek <39262379+dsvatunek@users.noreply.github.com>
2023-06-03 00:29:47 +0200

    Add files via upload

--- Orbital.js
+++ Orbital.js
@@ -8,31 +8,41 @@ $(document).ready(function() {
         var colorScheme = $(this).data('color-scheme');
         var viewer = loadMolecule(viewerId, file, colorScheme);
         viewers.push(viewer);
-        $(this).data('viewer', viewer);  // Store the viewer instance in the DOM element for reference later
+        $(this).data('viewer', viewer);
     });
 
     // Enable rotation and zoom synchronization
     var activeViewer = null;
+    var last_x, last_y;
     $('.mol_container').mousedown(function(event) {
         event.preventDefault();
         activeViewer = $(this).data('viewer');
-        var last_x = event.clientX;
-        var last_y = event.clientY;
+        last_x = event.clientX;
+        last_y = event.clientY;
+    });
 
-
-
-		$(document).mousemove(function(event) {
-			if (activeViewer) {
-				var dx = event.clientX - last_x;
-				var dy = event.clientY - last_y;
-				last_x = event.clientX;
-				last_y = event.clientY;
-				rotateViewers(viewers, dy, dx);
-			}
-		}).mouseup(function() {
-			$(document).off('mousemove');
-			activeViewer = null;
-		});
+    $(document).mousemove(function(event) {
+        if (activeViewer) {
+            var dx = event.clientX - last_x;
+            var dy = event.clientY - last_y;
+            last_x = event.clientX;
+            last_y = event.clientY;
+            activeViewer.rotate(1 * dy, {x: 1, y: 0});
+            activeViewer.rotate(1 * dx, {x: 0, y: 1});
+            activeViewer.render();
+        }
+    }).mouseup(function() {
+        if (activeViewer) {
+            var rotation = activeViewer.getRotation();
+            viewers.forEach(function(viewer) {
+                if (viewer !== activeViewer) {
+                    viewer.setRotation(rotation);
+                    viewer.render();
+                }
+            });
+        }
+        $(document).off('mousemove');
+        activeViewer = null;
     });
 
     // Enable zoom synchronization
@@ -40,10 +50,19 @@ $(document).ready(function() {
         event.preventDefault();
         var delta = event.originalEvent.deltaY;
         if (activeViewer) {
-            zoomViewers(viewers, activeViewer, delta);
+            activeViewer.zoom(1 + (1 * delta));
+            activeViewer.render();
+            var zoom = activeViewer.getZoom();
+            viewers.forEach(function(viewer) {
+                if (viewer !== activeViewer) {
+                    viewer.setZoom(zoom);
+                    viewer.render();
+                }
+            });
         }
     });
 });
+
 
 function loadMolecule(viewerId, file, colorScheme) {
     var viewer = $3Dmol.createViewer(viewerId);
